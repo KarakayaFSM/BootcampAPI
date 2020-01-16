9bdb91474d5c0e69a4803b322f5125ff677b650d
Salih Karakaya <salih.karakaya@stu.fsm.edu.tr>
2020-01-16 12:03:50 +0300

    initial express setup

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config({ path:'./config/config.env' });
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("hello from express");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT} in  ${process.env.NODE_ENV} mode`);
+});
