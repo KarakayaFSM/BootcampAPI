0a40e76d7cf4f3050cb0c1228ec4fb9ed78f345f
Salih Karakaya <salih.karakaya@stu.fsm.edu.tr>
2020-01-16 22:52:02 +0300

    established database connection with error checking

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const morgan = require('morgan');
-
-//Routes
-const bootcamps = require('./routes/bootcamps');
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+const colors = require('colors');
 
 //load env vars
-dotenv.config({ path:'./config/config.env' });
+dotenv.config({ path: "./config/config.env" });
+
+//connect to MongoDB
+connectDB();
+
+//Routes
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
-if(process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
-app.use('/api/v1/bootcamps',bootcamps);
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT} in  ${process.env.NODE_ENV} mode`);
+const server = app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT} mode: ${process.env.NODE_ENV}`.yellow);
+});
+
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`MongoDB Error: ${err.message}`.red);
+  //Close server & terminate process
+  server.close(() => process.exit(1));
 });
